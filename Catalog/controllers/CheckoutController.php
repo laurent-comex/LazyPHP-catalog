7a678964e544b8b83674a13d46e01e411428c2de
Laurent Comex <laurent.comex@sfr.fr>
2018-02-07 16:11:15 +0100

    maj

--- Catalog/controllers/CheckoutController.php
+++ Catalog/controllers/CheckoutController.php
@@ -224,7 +224,143 @@ class CheckoutController extends FrontController
 
 
 
-                $contents='Bonjour ' .  $this->current_user->firstname .', <br/><br/>
+                $contents=  ' <body>
+                   <table style="background-color:rgb(42, 55, 78)" width="100%" cellspacing="0" cellpadding="0" border="0">
+        <tbody>
+            <tr>
+                <td align="center" bgcolor="#000">
+                    <table cellspacing="0" cellpadding="0" border="0">
+                        <tbody>                            
+                            <tr>
+                                <td class="w640" width="640" height="10"></td>
+                            </tr>
+                            <tr>
+                                <td class="w640" width="640" height="20" align="center"> <a style="color:#ffffff; font-size:12px;" href="#"><span style="color:#ffffff; font-size:12px;">
+                                    Voir le contenu de ce mail en ligne</span></a> 
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="w640" width="640" height="10"></td>
+                            </tr>
+
+                            <!-- logo -->
+                            <tr class="pagetoplogo">
+                                <td class="w640" width="640">
+                                    <table class="w640" width="640" cellspacing="0" cellpadding="0" border="0" bgcolor="#F2F0F0">
+                                        <tbody>
+                                            <tr>
+                                                <td class="w30" width="30"></td>
+                                                <td class="w580" width="580" valign="middle" align="left">
+                                                    <div class="pagetoplogo-content">
+                                                        <img class="w580" style="text-decoration: none; display: block; color:#476688; font-size:30px;" src="/uploads/media/1/0/10_image.jpg" alt="Mon Logo" width="580" height="108">
+                                                    </div>
+                                                </td> 
+                                                <td class="w30" width="30"></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                                
+                            <!-- separateur horizontal -->
+                            <tr>
+                                <td class="w640" width="640" height="1" bgcolor="#d7d6d6"></td>
+                            </tr>
+
+                             <!-- contenu -->
+                            <tr class="content">
+                                <td class="w640" width="640" bgcolor="#ffffff">
+                                    <table class="w640" width="640" cellspacing="2" cellpadding="10" border="0">
+                                        <tbody>
+                                            <tr>
+                                                <td class="w30" width="30"></td>
+                                                <td class="w580" width="580">
+                                                    Bonjour ' .  $this->current_user->firstname .', <br/><br/> 
+                                                    Nous vous confirmons la réservation de votre séance de ' . $item->product->activity->label . ' - '. $label_slot .  ' avec le coach ' . $item->product->coach->firstname . '.<br/><br/>            
+                                                </td>
+                                                <td class="w30" width="30"></td>
+                                            </tr>
+ 
+                                            <tr>
+                                                <td class="w30" width="30"></td>
+                                                <td class="w580" width="580" bgcolor="#F7BF17">
+                                                    <br/>
+                                                    <strong>Infos séance :</strong> <br/>'
+                                                    //$datetimeInfos['startFormatted']
+                                                    . 'Le [Date] à [Heure] <br/>
+                                                     Rendez-vous au ' . $item->product->location->address . ' ' . $item->product->location->zip_code . ' ' . $item->product->location->city . '<br/> 
+                                                    <br/>
+                                                    </td>
+                                                   <td class="w30" width="30"></td>
+                                            </tr>
+                                            
+                                            <tr>
+                                                    <td class="w30" width="30"></td>
+                                                    <td class="w580" width="580">
+                                                        <br/>
+                                                        Il est recommandé d’arriver en tenue adaptée 5 minutes avant le début de la séance. <br/><br/> 
+
+                                                        Vous pouvez contacter le coach ' . $item->product->coach->firstname . ' au ' . $item->product->coach->phone . ' pour faciliter votre rencontre ou poser des questions sur la séance. <br/><br/> 
+                                                         ' .$confirmation_sentence . ' <br/>  <br/>
+                    
+                                                        <a href="http://fitnss.fr/pages/39" target="_blank">Consultez nos conditions générales d’utilisation</a> <br/><br/>
+                    
+                                                        Sportivement, <br/>
+                                                        L’équipe FITNSS <br/>
+                                                        <img style="text-decoration: none; display: block; color:#476688; font-size:30px;" src="/uploads/media/fitnss-logo_black-small.png" alt="Logo Fitnss" width="50" height="30">
+                                                    </td>
+                                                    <td class="w30" width="30"></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+
+                            <!--  separateur horizontal de 15px de  haut-->
+                            <tr>
+                                <td class="w640" width="640" height="15" bgcolor="#ffffff"></td>
+                            </tr>
+
+                            <!-- pied de page -->
+                            <tr class="pagebottom">
+                                <td class="w640" width="640">
+                                    <table class="w640" width="640" cellspacing="0" cellpadding="0" border="0" bgcolor="#c7c7c7">
+                                        <tbody>
+                                            <tr>
+                                                <td colspan="5" height="10"></td>
+                                            </tr>
+                                            <tr>
+                                                <td class="w30" width="30"></td>
+                                                <td class="w580" width="580" valign="top">
+                                                    <p class="pagebottom-content-left" align="right">
+                                                        <a style="color:#255D5C;" href="http://www.fitnss.fr"><span style="color:#255D5C;">www.fitnss.fr</span></a>
+                                                    </p>
+                                                </td>
+
+                                                <td class="w30" width="30"></td>
+                                            </tr>
+                                            <tr>
+                                                <td colspan="5" height="10"></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="w640" width="640" height="60"></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+</body>';
+
+
+/*
+                'Bonjour ' .  $this->current_user->firstname .', <br/><br/>
 
                     Nous vous confirmons la réservation de votre séance de ' . $item->product->activity->label . ' - '. $label_slot .  ' avec le coach ' . $item->product->coach->firstname . '.<br/><br/>
 
@@ -243,16 +379,16 @@ class CheckoutController extends FrontController
                     <a href="http://fitnss.fr/pages/39" target="_blank">Consultez nos conditions générales d’utilisation</a> <br/><br/>
 
                     Sportivement, <br/>
-                    L’équipe FITNSS ';
+                    L’équipe FITNSS ';*/
 
-                // var_dump($contents);die();
+                //echo $contents;die();
 
                 //mail pour les utilisateurs ayant réservé
-                Mail::send('hello@fitnss.fr', 'Contact', $this->current_user->email, $this->current_user->fullname, 'FITNSS, réservation de votre séance de '. $item->product->label . ' - ' . $item->product->label_slot , $contents);
+                Mail::send('hello@fitnss.fr', 'Contact', $this->current_user->email, $this->current_user->fullname, 'FITNSS, réservation de votre séance de '. $cart->items->product->label . ' - ' . $cart->items->product->label_slot , $contents);
 
-                Mail::send('hello@fitnss.fr', 'Contact', 'hello@fitnss.fr', 'Contact' , 'FITNSS, réservation d\'un séance de '. $item->product->label . ' - ' . $item->product->label_slot , 'Une séance a été réservée par' . $this->current_user->fullname .' Pour obtenir plus d\'information, rendez-vous dans le back office de votre site.');
+                Mail::send('hello@fitnss.fr', 'Contact', 'hello@fitnss.fr', 'Contact' , 'FITNSS, réservation d\'un séance de '. $cart->items->product->label . ' - ' . $cart->items->product->label_slot , 'Une séance a été réservée par' . $this->current_user->fullname .' Pour obtenir plus d\'information, rendez-vous dans le back office de votre site.');
 
-               /* Mail::send('hello@fitnss.fr', 'Contact', $item->product->coach->email, $item->product->coach->fullname, 'FITNSS, réservation d\'une place sur votre séance de '. $item->product->label . ' - ' . $item->product->label_slot , 'Une place de votre ' . $item->product->label . ' - ' . $item->product->label_slot .' séance a été réservée par' . $this->current_user->fullname .' ');
+               /* Mail::send('hello@fitnss.fr', 'Contact', $cart->items->product->coach->email, $item->product->coach->fullname, 'FITNSS, réservation d\'une place sur votre séance de '. $cart->items->product->label . ' - ' . $item->product->label_slot , 'Une place de votre ' . $cart->items->product->label . ' - ' . $item->product->label_slot .' séance a été réservée par' . $this->current_user->fullname .' ');
                 */
 
 
@@ -299,19 +435,46 @@ class CheckoutController extends FrontController
         $index = $index - 1 ;
 
         foreach ($cart->items as $idx => $item) {
-            echo $item->product->label . '<br />';
             if ($idx == $index) {
-                unset($item[$idx]);
+                // verifiez les dates
+                $delete = true;
+
+                if ($delete) {
+                    if($item->quantity > 1){
+                        $item->quantity--;
+                        $cart->save();
+                    } else {
+                       unset($cart->items[$idx]); 
+                    }
+
+/*
+                    if(isset($this->current_user->firstname)) {
+                        $this->addFlash('Réservation supprimé', 'success');
+
+                    Mail::send('hello@fitnss.fr', 'Contact', $this->current_user->email, $this->current_user->fullname, 'FITNSS, annulation de votre réservation' , 'Bonjour ' . $this->current_user->firstname . ',<br/><br/>
+
+                        Nous avons pris en compte votre annulation pour la séance de ' . $item->product->label . ' - ' . $item->product->label_slot . '  le [Date] à [Heure].<br/>
+
+                        Votre remboursement sera effectué suivant nos conditions générales d’utilisation dans un délai de 10 jours ouvrés. <br/>
+
+                        Pour réserver une autre séance FITNSS, <a href="http://fitnss.fr/slots/search" target="_blank"> cliquez ici</a> <br/><br/>
+
+                        Sportivement,<br/>
+                        L’équipe FITNSS.');
+
+                Mail::send('hello@fitnss.fr', 'Contact', 'hello@fitnss.fr', 'Contact' , 'FITNSS, annulation d\'une réservation', 'La séance de ' . $item->product->label . ' - ' . $item->product->label_slot . '  a été annulé par' . $this->current_user->fullname .' N\'oubliez pas de le recontacter pour le remboursement. ');
+
+                Mail::send('hello@fitnss.fr', 'Contact', $item->coach->email, $item->coach->fullname, 'FITNSS, annulation d\'une réservation',  'Une place de la  séance de ' . $item->product->label . ' - ' . $item->product->label_slot . '  a été annulé par' . $this->current_user->fullname .' ');
+*/
+
+
+
+
+                } else {
+                    $this->addFlash('Réservation non supprimé', 'error');
+                }
             }
-            echo $item->product->label . '<br />';
         }
-
-
-
-        echo $index;
-
-        $this->addFlash('Réservation supprimé', 'success');
-        // $this->redirect('user');
-
+        $this->redirect('catalog_checkout_cart');
     }
 }
